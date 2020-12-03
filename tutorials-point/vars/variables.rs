a3f8cc29c9f133f4c08ea94c7231e674bd86a41d
Constantinos Georgiou <constantgeorgiou@gmail.com>
2020-12-03 23:52:23 +0200

    add tutorials point directory

---
+++ tutorials-point/vars/variables.rs
@@ -0,0 +1,11 @@
+fn main(){
+    let company_name = "Uber";
+    let rating = 4.5;
+    let is_growing = true;
+    let icon = "🚗";
+
+    println!("Company       {}", company_name);
+    println!("  rating:     {}", rating);
+    println!("  growng:     {}", is_growing);
+    println!("  icon:       {}", icon);
+}
